6775051a85e0f9173fc8c8f6688261e6721b3dbe
Ganesh-Vodnala <vodnalaganesh2006@gmail.com>
2026-02-26 23:17:28 +0530

    Authentication system with JWT completed

--- server/server.js
+++ server/server.js
@@ -2,12 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
-
+const authRoutes=require("./routes/auth");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use("/api/auth",authRoutes);
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
